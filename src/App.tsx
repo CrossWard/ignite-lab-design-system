dbea611b82f36d2012e6ab6ec910213053e136ad
CrossWard <vinicius.silva.sdp@hotmail.com>
2022-10-14 10:17:36 -0300

    repositorio criado

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+export function App() {
+  return (
+    <>
+    <h1 className='font-bold text-2xl text-yellow-600'>Hello World</h1>
+    <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-zinc-700 hover:bg-cyan-300'>Enviar</button>
+    </>
+  )
+}
+
